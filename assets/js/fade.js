fc607831630b20eafb0e68c47977569670165bd1
Kajsa Tunius <kajsa@tunius.se>
2023-11-17 22:43:28 +0100

    no real changes

--- assets/js/fade.js
+++ assets/js/fade.js
@@ -2,6 +2,7 @@
     window.onload = () => {
         const anchors = document.querySelectorAll('a');
         const transition_el = document.querySelector('.transition');
+ 
       
         setTimeout(() => {
           transition_el.classList.remove('is_active');
